c64b22728e69de6da9dc6b4ef5b43032a37b25b6
JuniorSelister <junior.selister@gmail.com>
2023-03-13 19:19:37 -0300

    Handling Streams

--- 01-fundamentos-nodejs/streams/fundamentals.js
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -6,15 +6,33 @@ class OneToHundredStream extends Readable {
   _read() {
     const i = this.index++
 
-    if (i > 100) {
-      this.push(null)
-    } else {
-      const buf = Buffer.from(String(i))
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
 
-      this.push(buf)
-    }
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
   }
 }
 
 new OneToHundredStream()
-  .pipe(process.stdout)
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
